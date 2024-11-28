3047c058c27550e3010f5aafcdcc446c0f8d0789
LautaroAntriolo <lautaroantriololuis@gmail.com>
2024-11-28 11:09:54 -0300

    func estadisticas

--- practica/main.go
+++ practica/main.go
@@ -15,18 +15,31 @@ func main(){
 	//fechas.Recordatorio(2024,11,17,20,58,"A ver si esto funciona!")
 	//datos.Lecturas("/home/lautaro/Documentos/learningGo/practica/empleados_actualizados.csv")
 
-	// ejemplosM1.Volumenes("cono",4)
+	ejemplosM1.Volumenes("cono",4)
 	// ejemplosM1.NumeroMaGrande(4)
 	// ejemplosM1.Calculadora()
 	// ejemplosM1.Teatro(15)
 	// ejemplosM1.Descuento(1500,"Carnes")
 	// ejemplosM1.CalcularTotales()
-	ejemplosM1.DescomponerNumero(987)
-	ejemplosM1.DescomponerEInvertir(987)
+	// ejemplosM1.DescomponerNumero(987)
+	// ejemplosM1.DescomponerEInvertir(987)
 	fmt.Println("")
-	ejemplosM2.MayoNumero(1,2,3,4,5)
-	fmt.Print(ejemplosM2.AnioBisiesto(1992)) // true 
-	fmt.Print(ejemplosM2.AnioBisiesto(2000)) // true
-	fmt.Print(ejemplosM2.AnioBisiesto(1900)) // false
-	
+	// ejemplosM2.MayoNumero(1,2,3,4,5)
+	// fmt.Print(ejemplosM2.AnioBisiesto(1992)) // true 
+	// fmt.Print(ejemplosM2.AnioBisiesto(2000)) // true
+	// fmt.Print(ejemplosM2.AnioBisiesto(1900)) // false
+	// ejemplosM2.Descomponer(1234)
+	// ejemplosM2.Descomponer(12)
+	// ejemplosM2.Descomponer(12909)
+	// ejemplosM2.RepeticionNumero(12340012389)
+	// ejemplosM2.EleccionNumeroMasGrande(1,2,3,4,5,6,7,8,9,12,9,32,65.0)
+	vector := []int{7, 70, 1, 90, 30, 7, 21, 39, 50}
+	vector2 := []int{34, 7, 10, 9, 3, 60, 20, 30, 53,3}
+	// ejemplosM2.OrdenarVector(vector, true)
+	// ejemplosM2.OrdenarVectorOptimizado(vector, false)
+	vectorPares, vectorImpares := ejemplosM2.OrganizarVectoresEnParsEImpares(vector, vector2)
+	fmt.Println("El vector de numeros pares quedó: %v ", vectorPares)
+	fmt.Println("El vector de numeros impares quedó: %v ", vectorImpares)
+
+	ejemplosM2.Estadisticas(vector2)
 }
--- practica/programacionEstructuradaEnGo/ejemplosM2/ejemplosM2.go
+++ practica/programacionEstructuradaEnGo/ejemplosM2/ejemplosM2.go
@@ -2,7 +2,11 @@ package ejemplosM2
 
 import (
 	"fmt"
+	"math"
+	"sort"
+	"strconv"
 )
+
 /*1.Del capítulo “2. Estructuras de un programa en GO”, desarrollar los ejercicios
 propuestos en modo de funciones. HECHO*/
 /*2.Crear un programa que determine el mayor de 3, 4 y 5 números.*/
@@ -59,24 +63,232 @@ func ResultadoPrimo(fin int) bool {
 (según el número de dígitos) las unidades utilizadas con su cantidad. Por ejemplo:
 Se recibe 356, se debe retornar como respuesta:
 6 unidades
-
-42
-
 •5 decenas
-•3 centenas*/
+•3 centenas
+*/
+func Descomponer(numero int){
+	if numero <= 0 {
+		fmt.Println("Ingresá un número positivo.")
+		return
+	}
+
+	posiciones := []string{"unidades", "decenas", "centenas", "millares", "diezmillares", "cienmillares"}
+	i := 0
+
+	for numero > 0 {
+		digito := numero % 10
+		if digito > 0 {
+			fmt.Printf("%d %s\n", digito, posiciones[i])
+		}
+		numero /= 10 
+		i++;
+	}
+}
 
 /*6.Construir una función que reciba un número entero y cuente el número de
 veces que se repite un dígito (si existe).*/
+
+
+func RepeticionNumero(numero int) map[int]int {
+	
+	if numero <0{
+		fmt.Print("Ingresá un número válido. ")
+	}
+	
+	var numeroString string = strconv.Itoa(numero)
+	frecuencia := make(map[int]int)
+
+	for _, num := range numeroString{
+		// fmt.Println(num)
+
+		digito := int(num - '0')
+		// fmt.Println(digito)
+		frecuencia[digito]++
+	}
+	fmt.Println(frecuencia)
+	for digito, cantidad := range frecuencia {
+		fmt.Printf("%d aparece %d veces\n", digito, cantidad)
+	}
+	return frecuencia
+}
+
 /*7.Crear una función que permita llenar un vector de n digitas (por teclado) y
 calcule cual es el mayor de todos los elementos.*/
+
+func EleccionNumeroMasGrande(numeros ...int) int{
+	// Los múltiplas parámetros ya se representan como un slice.
+	masGrande := numeros[0]
+	
+	if len(numeros) == 0 {
+		return 0 
+	}
+
+	for _, numero := range numeros{
+		if numero > masGrande{
+			masGrande= numero
+		}
+	}
+	fmt.Printf("el número mas grande es %d", masGrande)
+	return masGrande
+}
 /*8.Crear una función que permita ordenar un vector de mayor a menor y
 menor a mayor.*/
+func OrdenarVectorOptimizado(vector []int, ascendente bool) []int {
+	n := len(vector)
+	for i := 0; i < n-1; i++ {
+		swapped := false // genero esta variable que me permite cortar antes si es necesario.
+		for j := 0; j < n-i-1; j++ {
+			if ascendente && vector[j] > vector[j+1] {
+				vector[j], vector[j+1] = vector[j+1], vector[j]
+				swapped = true // Cambia a true si al recorrerlo hace algún cambio.
+			}
+			if !ascendente && vector[j] < vector[j+1] {
+				vector[j], vector[j+1] = vector[j+1], vector[j]
+				swapped = true
+			}
+		}
+		// Si no hay cambios, entonces el slices ya está ordenada y salimos de la función.
+		if !swapped {
+			break
+		}
+	}
+	fmt.Print(vector)
+	return vector
+}
+func OrdenarVector(vector []int, ascendente bool) []int{
+
+	if ascendente{
+		sort.Ints(vector)
+	}else{
+		sort.Sort(sort.Reverse(sort.IntSlice(vector)))
+	}
+	// fmt.Print(vector)
+	return vector
+}
+
 /*9.Construir un programa que, dados dos vectores, devuelva dos nuevos
 vectores que junten los números pares e impares de cada uno.*/
+func OrganizarVectoresEnParsEImpares(vector1, vector2 []int) ([]int, []int){
+
+	var vectorPares []int
+	var vectorImpares []int
+
+	for _, numero := range vector1{
+		if numero % 2 == 0{
+			vectorPares = append(vectorPares, numero)
+		}else{
+			vectorImpares = append(vectorImpares, numero)
+		}
+	}
+	for _, numero := range vector2{
+		if numero % 2 == 0{
+			vectorPares = append(vectorPares, numero)
+		}else{
+			vectorImpares = append(vectorImpares, numero)
+		}
+	}
+	return vectorPares, vectorImpares
+}
+
 /*10. Crear un programa capaz de recibir una lista de datos y calcular la media,
 mediana, moda y desviación estándar del conjunto de datos.*/
+func mediaDeUnaLista(lista []int) float32 {
+	var suma int 
+	for _, num := range lista{
+		suma += num
+	}
+	var resultado float64 = float64(suma) / float64(len(lista))
+	return float32(resultado)
+}
+func medianaDeUnaLista(lista []int)float32{
+	var posicionMedia int = len(lista)/2
+	var listaOrdenada []int = OrdenarVector(lista, true)
+	valorMediano := listaOrdenada[posicionMedia]
+	return float32(valorMediano)
+}
+func modaDeUnaLista (lista []int)float32{
+
+	organizacion := make(map[int]int)
+	var moda int 
+	var frecuanciaMaxima int
+
+	for _, num := range lista{
+		organizacion[num]++
+	}
+
+	for numero, frecuencia := range organizacion{
+		if frecuencia > frecuanciaMaxima{
+			moda = numero
+			frecuanciaMaxima = frecuencia
+		}
+	}
+	return float32(moda)
+
+}
+// Puede haber mas de una moda. EL código quedaría algo así: 
+func modasDeUnaLista(lista []int) []int {
+	
+	organizacion := make(map[int]int)
+	var frecuanciaMaxima int
+	var modas []int
+
+	for _, num := range lista {
+		organizacion[num]++
+	}
+
+	for numero, frecuencia := range organizacion {
+		if frecuencia > frecuanciaMaxima {
+			frecuanciaMaxima = frecuencia
+			modas = []int{numero}
+		} else if frecuencia == frecuanciaMaxima {
+			modas = append(modas, numero)
+		}
+	}
+
+	return modas
+}
+func desviacionEstandar(lista []int) float32{
+
+	tamanio := len(lista)
+	if tamanio == 0 {
+		return 0 
+	}
+
+	media := mediaDeUnaLista(lista)
+	var sumaCuadrados float32
+
+	for _, num := range lista {
+		valor := float32(num) - media 
+		sumaCuadrados += valor * valor
+	}
+
+	var desEst float64 = math.Sqrt(float64(sumaCuadrados) / float64(tamanio))
+
+	return float32(desEst)
+}
+
+func Estadisticas(lista []int) (float32, float32, float32){
+	
+	var media float32 = mediaDeUnaLista(lista)
+	fmt.Printf("La media es %.2f\n", media)
+	
+	var mediana float32 = medianaDeUnaLista(lista)
+	fmt.Printf("la mediana es %.2f\n", mediana)
+
+	var moda float32 = modaDeUnaLista(lista)
+	fmt.Printf("la moda  es %.2f\n", moda)
+	
+	var desEstandar float32 = desviacionEstandar(lista)
+	fmt.Printf("la desviacion estandar es %.2f\n",desEstandar)
+
+	return media, mediana, moda
+
+}
+
 /*11.Construir una función que, dadas dos matrices, calcule la suma y
 multiplicación de sus elementos. (Tener en cuenta propiedades de las matrices)*/
+
+
 /*12.Construir una función en donde dada una matriz, calcule su matriz
 transformada. (Tener en cuenta propiedades de las matrices)*/
 
